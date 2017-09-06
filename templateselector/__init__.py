1c42baf934f38bb09b0939c67512057e33cacabb
Keryn Knight <keryn@kerynknight.com>
2017-09-06 10:55:30 +0100

    Bump version: 0.2.4 → 0.2.5

--- templateselector/__init__.py
+++ templateselector/__init__.py
@@ -2,10 +2,10 @@
 from __future__ import absolute_import
 from __future__ import unicode_literals
 
-__version_info__ = '0.2.4'
-__version__ = '0.2.4'
-version = '0.2.4'
-VERSION = '0.2.4'
+__version_info__ = '0.2.5'
+__version__ = '0.2.5'
+version = '0.2.5'
+VERSION = '0.2.5'
 
 def get_version():
     return version  # pragma: no cover
